85472ba1dde2a3f74d5fba13443b1d89552cc25d
Roman Marquez <rm426.iso@gmail.com>
2019-03-18 14:27:11 -0500

    Add files via upload

---
+++ FAScrape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace FurScrape
+{
+    class FAScrape
+    {
+        static void Main(string[] args)
+        {
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.furaffinity.net/journals/rudragon/");
+
+            var htmlNodes = doc.DocumentNode.SelectNodes("//*[@class='maintable page-journals-list']");
+
+            foreach (var node in htmlNodes)
+            {
+                Console.WriteLine("Writing to scrape.txt");
+                System.IO.File.WriteAllText("scrape.txt", node.InnerText);
+                Console.WriteLine("scrape.txt has been written to.");
+            }
+        }
+    }
+}
